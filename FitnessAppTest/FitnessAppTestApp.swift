dcd773f3dcc37b271562cdea5761c78732483e84
Vlad Matei <vlad.mateei@gmail.com>
2022-01-02 17:11:08 +0200

    Initial Commit

---
+++ FitnessAppTest/FitnessAppTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppTestApp.swift
+//  FitnessAppTest
+//
+//  Created by Vlad Matei on 02/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
